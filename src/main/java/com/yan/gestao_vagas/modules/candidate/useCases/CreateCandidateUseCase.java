9fb7a77a5513a875b3b9d39c8e79029a9e4b1849
Yan Lima Barbosa <yanbr763@gmail.com>
2025-05-11 13:35:13 -0300

    feat: create useCase for Candidate

---
+++ src/main/java/com/yan/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -0,0 +1,23 @@
+package com.yan.gestao_vagas.modules.candidate.useCases;
+
+import com.yan.gestao_vagas.exceptions.UserFoundException;
+import com.yan.gestao_vagas.modules.candidate.CandidateEntity;
+import com.yan.gestao_vagas.modules.candidate.CandidateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateCandidateUseCase {
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    public CandidateEntity execute(CandidateEntity candidateEntity) {
+        this.candidateRepository
+                .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
+                .ifPresent((user) -> {
+                    throw new UserFoundException();
+                });
+
+        return this.candidateRepository.save(candidateEntity);
+    };
+}
