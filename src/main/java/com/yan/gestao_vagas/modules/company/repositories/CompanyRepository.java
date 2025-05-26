ddb0fec6a23619a5ac8ed1ac023fe2d071f35f3c
Yan Lima Barbosa <yanbr763@gmail.com>
2025-05-25 22:14:54 -0300

    feat: add CreateCompanyUseCase

---
+++ src/main/java/com/yan/gestao_vagas/modules/company/repositories/CompanyRepository.java
@@ -0,0 +1,11 @@
+package com.yan.gestao_vagas.modules.company.repositories;
+
+import com.yan.gestao_vagas.modules.company.entities.CompanyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CompanyRepository extends JpaRepository<CompanyEntity, UUID> {
+    Optional<CompanyEntity> findByUsernameOrEmail(String username, String email);
+}
---
+++ src/main/java/com/yan/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
@@ -0,0 +1,24 @@
+package com.yan.gestao_vagas.modules.company.useCases;
+
+import com.yan.gestao_vagas.exceptions.UserFoundException;
+import com.yan.gestao_vagas.modules.company.entities.CompanyEntity;
+import com.yan.gestao_vagas.modules.company.repositories.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateCompanyUseCase {
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    public CompanyEntity execute(CompanyEntity companyEntity) {
+        this.companyRepository
+                .findByUsernameOrEmail(companyEntity.getUsername(), companyEntity.getEmail())
+                .ifPresent((user) -> {
+                    throw new UserFoundException();
+                });
+
+        return this.companyRepository.save(companyEntity);
+    }
+}
