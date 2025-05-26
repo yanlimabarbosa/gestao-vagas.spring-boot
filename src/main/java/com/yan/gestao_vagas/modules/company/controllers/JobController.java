8df4a4b7fd07f0cbebd7690f7601a88c86ddfc44
Yan Lima Barbosa <yanbr763@gmail.com>
2025-05-25 23:35:54 -0300

    feat: add JobController

---
+++ src/main/java/com/yan/gestao_vagas/modules/company/controllers/JobController.java
@@ -0,0 +1,28 @@
+package com.yan.gestao_vagas.modules.company.controllers;
+
+import com.yan.gestao_vagas.modules.company.entities.JobEntity;
+import com.yan.gestao_vagas.modules.company.useCases.CreateJobUseCase;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/job")
+public class JobController {
+    @Autowired
+    private CreateJobUseCase createJobUseCase;
+
+    @PostMapping("/")
+    public ResponseEntity<Object> create(@Valid @RequestBody JobEntity jobEntity) {
+        try {
+            var result = this.createJobUseCase.execute(jobEntity);
+            return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
