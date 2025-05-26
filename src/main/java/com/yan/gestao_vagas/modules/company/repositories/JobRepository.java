6df89991b2d3b4d46842df2d4a61a4d4db7a7011
Yan Lima Barbosa <yanbr763@gmail.com>
2025-05-25 23:35:28 -0300

    feat: create JobRepository

---
+++ src/main/java/com/yan/gestao_vagas/modules/company/repositories/JobRepository.java
@@ -0,0 +1,9 @@
+package com.yan.gestao_vagas.modules.company.repositories;
+
+import com.yan.gestao_vagas.modules.company.entities.JobEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface JobRepository extends JpaRepository<JobEntity, UUID> {
+}
