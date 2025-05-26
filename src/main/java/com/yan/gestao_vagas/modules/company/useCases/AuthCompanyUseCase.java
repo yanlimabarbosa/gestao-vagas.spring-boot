83edbbab07a370b24a56c8b980dffb6950acb357
Yan Lima Barbosa <yanbr763@gmail.com>
2025-05-26 03:53:06 -0300

    feat: create AuthCompanyUseCase

---
+++ src/main/java/com/yan/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -0,0 +1,46 @@
+package com.yan.gestao_vagas.modules.company.useCases;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.yan.gestao_vagas.modules.company.dto.AuthCompanyDTO;
+import com.yan.gestao_vagas.modules.company.repositories.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.naming.AuthenticationException;
+
+@Service
+public class AuthCompanyUseCase {
+
+    @Value("${security.token.secret}")
+    private String secretKey;
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public String execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
+        var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow(
+                () -> new UsernameNotFoundException("Usuário ou senha incorretos")
+        );
+
+        var passwordMatches = this.passwordEncoder.matches(authCompanyDTO.getPassword(), company.getPassword());
+
+        if (!passwordMatches) {
+            throw new UsernameNotFoundException("Usuário ou senha incorretos");
+        }
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+
+        var token = JWT.create().withIssuer("yan_vagas")
+                .withSubject(company.getId().toString())
+                .sign(algorithm);
+
+        return token;
+    }
+}
