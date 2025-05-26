bc9f5be6fcfdf9140c343157049c3a3d1b35ff6f
Yan Lima Barbosa <yanbr763@gmail.com>
2025-05-25 22:14:40 -0300

    feat: create job entity

---
+++ src/main/java/com/yan/gestao_vagas/modules/company/entities/JobEntity.java
@@ -0,0 +1,29 @@
+package com.yan.gestao_vagas.modules.company.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name = "job")
+@Data
+public class JobEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String description;
+    private String benefits;
+    private String level;
+
+    @ManyToOne()
+    @JoinColumn(name = "company_id", insertable = false, updatable = false)
+    private CompanyEntity companyEntity;
+
+    @Column(name = "company_id")
+    private UUID companyId;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
