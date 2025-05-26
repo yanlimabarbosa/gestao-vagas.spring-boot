7fc4fe1f7996d1910a8986a5b243f51a022b9105
Yan Lima Barbosa <yanbr763@gmail.com>
2025-05-26 03:09:50 -0300

    feat: encode password on company creation

--- src/main/java/com/yan/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
+++ src/main/java/com/yan/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
@@ -4,6 +4,7 @@ import com.yan.gestao_vagas.exceptions.UserFoundException;
 import com.yan.gestao_vagas.modules.company.entities.CompanyEntity;
 import com.yan.gestao_vagas.modules.company.repositories.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,12 +13,18 @@ public class CreateCompanyUseCase {
     @Autowired
     private CompanyRepository companyRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CompanyEntity execute(CompanyEntity companyEntity) {
         this.companyRepository
                 .findByUsernameOrEmail(companyEntity.getUsername(), companyEntity.getEmail())
                 .ifPresent((user) -> {
                     throw new UserFoundException();
                 });
+
+        var encodedPassword = passwordEncoder.encode(companyEntity.getPassword());
+        companyEntity.setPassword(encodedPassword);
 
         return this.companyRepository.save(companyEntity);
     }
