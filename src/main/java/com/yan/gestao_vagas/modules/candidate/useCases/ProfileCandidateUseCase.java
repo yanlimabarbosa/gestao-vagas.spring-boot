4a6d95c3e28cc47da62cfffa8396adb847e29845
Yan Lima Barbosa <yanbr763@gmail.com>
2025-06-06 00:47:30 -0300

    feat (candidate): add ProfileCandidateUseCase

---
+++ src/main/java/com/yan/gestao_vagas/modules/candidate/useCases/ProfileCandidateUseCase.java
@@ -0,0 +1,32 @@
+package com.yan.gestao_vagas.modules.candidate.useCases;
+
+import com.yan.gestao_vagas.modules.candidate.dto.ProfileCandidateResponseDTO;
+import com.yan.gestao_vagas.modules.candidate.repositories.CandidateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class ProfileCandidateUseCase {
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    public ProfileCandidateResponseDTO execute(UUID idCandidate) {
+        var candidate = this.candidateRepository.findById(idCandidate)
+                .orElseThrow(() -> {
+                    throw new UsernameNotFoundException("User not found");
+                });
+
+        var candidateDTO = ProfileCandidateResponseDTO.builder()
+                .description(candidate.getDescription())
+                .username(candidate.getUsername())
+                .email(candidate.getEmail())
+                .name(candidate.getName())
+                .id(candidate.getId())
+                .build();
+
+        return candidateDTO;
+    }
+}
