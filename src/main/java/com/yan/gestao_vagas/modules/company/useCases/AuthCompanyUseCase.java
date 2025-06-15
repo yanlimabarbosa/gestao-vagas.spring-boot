001c441550223956056bf0e4d606222a3feb5853
Yan Lima Barbosa <yanbr763@gmail.com>
2025-06-15 03:27:06 -0300

    feat (company): add company authentication and roles

--- src/main/java/com/yan/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/com/yan/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -3,6 +3,7 @@ package com.yan.gestao_vagas.modules.company.useCases;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.yan.gestao_vagas.modules.company.dto.AuthCompanyDTO;
+import com.yan.gestao_vagas.modules.company.dto.AuthCompanyResponseDTO;
 import com.yan.gestao_vagas.modules.company.repositories.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Service;
 import javax.naming.AuthenticationException;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.List;
 
 @Service
 public class AuthCompanyUseCase {
@@ -26,7 +28,7 @@ public class AuthCompanyUseCase {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public String execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
+    public AuthCompanyResponseDTO execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
         var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow(
                 () -> new UsernameNotFoundException("Usuário ou senha incorretos")
         );
@@ -42,8 +44,16 @@ public class AuthCompanyUseCase {
         var token = JWT.create().withIssuer("yan_vagas")
                 .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
                 .withSubject(company.getId().toString())
+                .withClaim("roles", List.of("COMPANY"))
                 .sign(algorithm);
 
-        return token;
+        var expiresIn = Instant.now().plus(Duration.ofHours(2));
+
+        var authCompanyResponseDTO = AuthCompanyResponseDTO.builder()
+                .access_token(token)
+                .expires_in(expiresIn.toEpochMilli())
+                .build();
+
+        return authCompanyResponseDTO;
     }
 }
--- src/main/java/com/yan/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/com/yan/gestao_vagas/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.yan.gestao_vagas.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,9 +12,13 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
     @Autowired
     private SecurityFilter securityFilter;
+
+    @Autowired
+    private SecurityCandidateFilter securityCandidateFilter;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -21,11 +26,12 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/candidate/").permitAll()
                             .requestMatchers("/company/").permitAll()
-                            .requestMatchers("/auth/company").permitAll()
+                            .requestMatchers("/company/auth").permitAll()
                             .requestMatchers("/candidate/auth").permitAll();
 
                     auth.anyRequest().authenticated();
                 })
+                .addFilterBefore(securityCandidateFilter, BasicAuthenticationFilter.class)
                 .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
         return http.build();
     }
